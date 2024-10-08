19c2954ecf80a36f505d12034bcf3cd22ae139a5
Bob Zhang <141781876+BobZhang26@users.noreply.github.com>
2024-10-08 08:35:14 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,43 +1,70 @@
+
 """
-Transforms and loads data to local SQlite3 database
+Transforms and Loads data into Azure Databricks
 """
-import sqlite3
-import csv
-# import os
+import os
+from databricks import sql
+import pandas as pd
+from dotenv import load_dotenv
 
 
-def load(dataset="rainfall.csv"):
-    # print the full working directory
-    # print(os.getcwd())
-    payload = csv.reader(open(dataset, newline="",encoding='utf-8'), delimiter=",")
-    conn = sqlite3.connect("rainfall.db")
-    c = conn.cursor()
-    # create new table for the database rainfall
-    c.execute("DROP TABLE IF EXISTS rainfall")
-    c.execute(
-        """
-              CREATE TABLE rainfall (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              Date DATE,
-              POONDI FLOAT,
-              CHOLAVARAM FLOAT,
-              REDHILLS FLOAT,
-              CHEMBARAMBAKKAM FLAOT)
-              """
-    )
-    # insert values
-    c.executemany(
-        """
-                  INSERT INTO rainfall 
-                  (Date,POONDI,CHOLAVARAM,REDHILLS,CHEMBARAMBAKKAM)
-                  VALUES(?,?,?,?,?)
-                  """,
-        payload,
-    )
-    conn.commit()
-    conn.close()
-    return "rainfall.db"
+# load the csv file and insert into databricks
+def load(dataset="data/births2000.csv", dataset2="data/births1994.csv"):
+    """Transforms and Loads data into the local databricks database"""
+    df = pd.read_csv(dataset, delimiter=",", skiprows=1)
+    df2 = pd.read_csv(dataset2, delimiter=",", skiprows=1)
+    load_dotenv()
+    server_h = os.getenv("SERVER_HOSTNAME")
+    access_token = os.getenv("ACCESS_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        # INSERT TAKES TOO LONG
+        # c.execute("DROP TABLE IF EXISTS births2000DB")
+        c.execute("SHOW TABLES FROM default LIKE 'births2000*'")
+        result = c.fetchall()
+        # takes too long so not dropping anymore
+        # c.execute("DROP TABLE IF EXISTS births1994DB")
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS births2000DB (
+                    id int,
+                    year int,
+                    month int,
+                    date_of_month int,
+                    day_of_week int,
+                    births int
+                )
+            """
+            )
+            # insert
+            for _, row in df.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO births2000DB VALUES {convert}")
+        c.execute("SHOW TABLES FROM default LIKE 'births1994*'")
+        result = c.fetchall()
+        # c.execute("DROP TABLE IF EXISTS births1994DB")
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS births1994DB (
+                    id int,
+                    year int,
+                    month int,
+                    date_of_month int,
+                    day_of_week int,
+                    births int
+                )
+                """
+            )
+            for _, row in df2.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO births1994DB VALUES {convert}")
+        c.close()
 
-
-if __name__ == "__main__":
-    load()
+    return "success"
